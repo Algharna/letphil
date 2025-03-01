36659f667f85bdf774380ff08e7b6310f0beedc8
Algharna <algharna@gmail.com>
2025-02-28 21:28:06 -0600

    pkmn upload

--- review-http-requests/homework/scripts/scripts.js
+++ review-http-requests/homework/scripts/scripts.js
@@ -1,13 +1,53 @@
-console.log("hello");
+const URL = "https://pokeapi.co/api/v2/pokemon/";
+const search = document.getElementById("search");
+pokemonSearch = document.getElementById("pokemonSearch");
+const defaultPokemon = "Jirachi";
+const pokedex = document.getElementById("pokedex");
 
-async function getPokemon(pokemonName) {
+async function fetchPokemon(pokemon = defaultPokemon) {
+  searchValue = pokemonSearch.value.toLowerCase();
+  let query = "";
+
+  if (searchValue) {
+    query = searchValue;
+  } else if (pokemon) {
+    query = pokemon;
+  }
+
   try {
-    const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
-    );
+    const response = await fetch(URL + query);
+    if (!response.ok) {
+      throw new Error("Pokemon not found!");
+    }
+    const data = await response.json();
+    console.log(data);
 
-    resoponse.ok;
-  } catch (error) {}
+    const pokemonObj = {
+      name: data.name,
+      id: data.id,
+      image: data.sprites.front_default,
+      type: data.types.map((t) => t.type.name).join(", "),
+      height: (data.height * 0.1).toFixed(1),
+      weight: (data.weight * 0.1).toFixed(1),
+      abilities: data.abilities.map((a) => a.ability.name).join(", "),
+    };
+    console.log(pokemonObj);
+    renderPokemon(pokemonObj);
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
-getPokemon("jirachi");
+function renderPokemon({ name, id, image, type, height, weight, abilities }) {
+  pokedex.innerHTML = `
+  <h2>${name}, Pokedex #: ${id}</h2> 
+  <br />
+  <img src="${image}" alt="${name}" />
+  <p>Type(s): ${type}</p>
+  <p>Height: ${height} m,
+  Weight: ${weight} kg</p>
+  <p>Abilitie(s): ${abilities}</p>
+  `;
+}
+
+search.addEventListener("click", fetchPokemon);
