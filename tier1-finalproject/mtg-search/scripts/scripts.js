90e7aa61b8267527b753f1dd7737c049135367d3
Algharna <algharna@gmail.com>
2025-03-24 17:30:42 -0500

    changes

--- tier1-finalproject/mtg-search/scripts/scripts.js
+++ tier1-finalproject/mtg-search/scripts/scripts.js
@@ -12,27 +12,6 @@ const headers = {
 
 axios.defaults.baseURL = BASE;
 
-// axios.get()
-
-// fetch(`${base}${paths.random}`)
-//function for getting 1 Random Card
-async function getRandomCards() {
-  try {
-    const res = await axios.get(paths.random, { headers });
-    console.log(res.data);
-
-    const cardObj = createCardObj(res.data.name, res.data.image_uris.normal);
-    console.log(cardObj);
-    //Displays Result
-    document.querySelector("#results pre").innerHTML = displayResults(
-      cardObj.card_name,
-      cardObj.card_img
-    );
-  } catch (error) {
-    // handle error better
-    alert(error.toString());
-  }
-}
 //function for search card
 async function searchCard(q) {
   try {
@@ -43,19 +22,38 @@ async function searchCard(q) {
       },
       { headers }
     );
+    console.log(res.data.data);
 
-    const exactMatch = res.data.data.find((c) => c.name === q);
+    const exactMatch = res.data.data.find(
+      (c) => c.name.toLowerCase() === q.toLowerCase()
+    );
 
+    console.log("exactMatch", exactMatch);
+    // console.log(res.data.data);
     if (!exactMatch) {
       for (let i = 0; i < res.data.data.length; i++) {
         const current = res.data.data[i];
-        document.querySelector("#results pre").innerHTML += displayResults(
+        const cardObj = createCardObj(
           current.name,
-          current.image_uris?.normal ?? ""
+          current.image_uris,
+          current.layout
         );
+        console.log(cardObj);
+        console.log(cardObj.card_layout);
+
+        if (cardObj.card_layout === "transform") {
+          document.querySelector("#results div").innerHTML += displayResults(
+            cardObj.card_name
+          );
+        } else if (cardObj.card_layout !== "transform") {
+          document.querySelector("#results div").innerHTML += displayResults(
+            cardObj.card_name,
+            cardObj.card_img?.normal ?? ""
+          );
+        }
       }
     } else {
-      document.querySelector("#results pre").innerHTML += displayResults(
+      document.querySelector("#results div").innerHTML = displayResults(
         exactMatch.name,
         exactMatch.image_uris?.normal ?? ""
       );
@@ -65,33 +63,77 @@ async function searchCard(q) {
     alert(error.toString());
   }
 }
+//function for getting 1 Random Card
+async function getRandomCards() {
+  try {
+    const res = await axios.get(paths.random, { headers });
 
+    const cardObj = createCardObj(
+      res.data.name,
+      res.data.image_uris,
+      res.data.layout
+    );
+    console.log(cardObj);
+    //Displays Result
+    console.log(cardObj.card_layout);
+    if (cardObj.card_layout === "transform") {
+      document.querySelector("#results div").innerHTML = displayResults(
+        cardObj.card_name
+      );
+    } else if (cardObj.card_layout !== "transform") {
+      document.querySelector("#results div").innerHTML = displayResults(
+        cardObj.card_name,
+        cardObj.card_img?.normal ?? ""
+      );
+    }
+  } catch (error) {
+    // handle error better
+    alert(error.toString());
+  }
+}
 search.onclick = async function () {
-  document.querySelector("#results pre").innerHTML = "";
-  const value = cardSearch.value;
-  await searchCard(value);
-  cardSearch.value = "";
+  // if(cardSearch.value.trim() === "") {
+
+  // }
+  const loader = document.createElement("div");
+  loader.classList.add("loader");
+  try {
+    document.querySelector("#results div").innerHTML = "";
+    document.querySelector("#results div").appendChild(loader);
+    const value = cardSearch.value;
+    await searchCard(value);
+    cardSearch.value = "";
+  } catch (error) {
+    console.error("error =", error);
+  } finally {
+    document.querySelector("#results div").firstElementChild.remove();
+  }
 };
 
-function createCardObj(name, img) {
+function createCardObj(name, img, layout) {
   const cardObj = {
     card_name: name,
     card_img: img,
+    card_layout: layout,
   };
   return cardObj;
 }
+//add card to favorites in localstorage
+function addToFavorites() {}
+
+//renders favorites from localstorage
+function displayFavorites() {}
 
 //display Results
 function displayResults(name, img) {
   if (img) {
     return `
-      <h2>${name}</hr>
+      <h2>${name}</hr> <button id="favorites">+</button>
       <br />
       <img src="${img}" alt="${name}" />
       `;
   } else {
-    return `<h2>${name}</h2>`;
+    return `<h2>${name}</h2>
+    <img src="https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/f/f8/Magic_card_back.jpg" alt="${name}" />`;
   }
 }
-
-// getRandomCards();
